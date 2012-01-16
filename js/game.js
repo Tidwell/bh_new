293904a7759aa8f9494b5284093604680b2bf0e1
Aaron Tidwell <atidwell@businessinsider.com>
2012-01-15 23:06:36 -0500

    organization

--- js/game.js
+++ js/game.js
@@ -3,6 +3,24 @@ var game = function(opt) {
   this.entities = [];
   this.stage = opt.stage;
 }
+game.prototype.addEntity = function(entity) {
+  entity.com = this.com;
+  this.entities.push(entity);
+};
+
+game.prototype.removeEntity = function(opt){
+  var i = 0;
+  var toRemove;
+  self.entities.forEach(function(entity){
+    if (entity.id==opt.id) {
+      toRemove = i;
+    }
+    i++;
+  })
+  self.entities.remove(i);
+};
+
+//main game loop
 game.prototype.loop = function() {
   var self = this;
   var d = new Date();
@@ -11,27 +29,19 @@ game.prototype.loop = function() {
     entity.update(t);
   });
   setTimeout(function() {self.loop()}, 20);
-}
+};
+
 game.prototype.init = function() {
   var self = this;
+  //inititalize each of the entities
   this.entities.forEach(function(entity) {
     entity.stage = self.stage;
     entity.init();
   });
+  //when we get a destruction notice from an entity, we need to
+  //remove it from our array
+  this.com.bind('removeEntity', self.removeEntity);
+  
+  /***Inititalize the Main Game Loop***/
   setTimeout(function() {self.loop()}, 20);
-  this.com.bind('removeEntity', function(opt){
-    var i = 0;
-    var toRemove;
-    self.entities.forEach(function(entity){
-      if (entity.id==opt.id) {
-        toRemove = i;
-      }
-      i++;
-    })
-    self.entities.remove(i);
-  })
-}
-game.prototype.addEntity = function(entity) {
-  entity.com = this.com;
-  this.entities.push(entity);
-}
+};
