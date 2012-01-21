9d5c7d053339dbd09a7efbe68d596182a5257faf
Aaron Tidwell <atidwell@businessinsider.com>
2012-01-21 18:29:17 -0500

    beginning of persistance

--- js/world.js
+++ js/world.js
@@ -1,9 +1,8 @@
 var world = function() {
   this.worldEl = $('#container');
   var self = this;
-  $('.nav').on('click', 'li', function(e){
-    self.nav(e,$(this))
-  });
+  self.getData();
+  self.bindDom();
 }
 world.prototype.nav = function(e,el) {
   var self = this;
@@ -18,73 +17,51 @@ world.prototype.nav = function(e,el) {
 
   }
 };
+world.prototype.getData = function() {
+  this.userData = $.jStorage.get('bh',{
+    activeUnits: this.defaultUnits,
+    reserveUnits: [],
+    map: [],
+    inventory: []
+  });
+  this.saveData();
+};
+
+world.prototype.saveData = function() {
+  $.jStorage.set('bh', this.userData);
+};
+
+world.prototype.bindDom = function() {
+  var self = this;
+  $('.nav').on('click', 'li', function(e){
+    self.nav(e,$(this))
+  });
+
+}
 
 world.prototype.startRandom = function() {
   var self = this;
   var stage = $('.stage');
   var t = new template;
-  
   self.worldEl.hide();
-  stage.append(t.ship('ship1'))
-    .append(t.ship('ship2'))
-    .append(t.ship('bx bx1 enemy'))
-          
-  $('.actionbars').append(t.actionbar('ship1'))
-    .append(t.actionbar('ship2'))
-        
-  $('.chars').append(t.charInfo('ship1','images/green_ship.png'))
-    .append(t.charInfo('ship2','images/green_healer.png'))
   
   var com = new communicator();
-  com.bind('gameOver',function(winner) {
-    stage.remove();
-    self.worldEl.show();
-  });
   var bh = new game({stage: stage, com: com});
-  var char1 = new entity({
-      id: 'ship1',
-      controllable: true,
-      startX: 20,
-      startY: 250,
-      height: 33, //px
-      width: 43, //px
-      rate: 200, //px/s
-      health: 100,
-      weapon: {
-        damage: 15,
-        range: 120, //px
-        cooldown: 2000, //ms
-        target: 'npc'
-      },
-      domEl: $('.entity.ship1'),
-      infoEl: $('.chars .ship1'),
-      actionbarEl: $('.actionbars .ship1'),
-      selectKey: '1',
-      attackKey: 'a',
-      type: 'pc'
-  });
-  var char2 = new entity({
-      id: 'healer',
-      controllable: true,
-      startX: 50,
-      startY: 300,
-      height: 36, //px
-      width: 43, //px
-      rate: 200, //px/s
-      health: 100,
-      weapon: {
-        damage: -5,
-        range: 120, //px
-        cooldown: 2500, //ms
-        target: 'pc'
-      },
-      domEl: $('.entity.ship2'),
-      infoEl: $('.chars .ship2'),
-      actionbarEl: $('.actionbars .ship2'),
-      selectKey: '2',
-      attackKey: 'a',
-      type: 'pc'
-  });
+  
+  self.userData.activeUnits.forEach(function(unit,i){
+    //make dom els
+    stage.append(t.ship(unit.id));
+    $('.actionbars').append(t.actionbar(unit.id));
+    $('.chars').append(t.charInfo(unit.id,unit.img));
+    //set unit dom info
+    unit.domEl = $('.entity.'+unit.id)
+    unit.infoEl = $('.chars .'+unit.id)
+    unit.actionbarEl = $('.actionbars .'+unit.id)
+    //add to game
+    bh.addEntity(new entity(unit))
+  })
+  
+  stage.append(t.ship('bx bx1 enemy'))
   var enemy = new entity({
       controllable: false,
       startX: 400,
@@ -104,10 +81,57 @@ world.prototype.startRandom = function() {
       validTargets: ['ship1','healer'],
       type: 'npc'
   });
-  bh.addEntity(char1);
-  bh.addEntity(char2);
-  bh.addEntity(enemy);
+  bh.addEntity(enemy);  
+  self.bindEvents(com,stage);
   var rend = new renderer({game: bh});
   bh.init();
   rend.init();
 };
+
+world.prototype.bindEvents = function(com,stage) {
+  var self = this;
+  com.bind('gameOver',function(winner) {
+    stage.remove();
+    self.worldEl.show();
+  });
+}
+
+world.prototype.defaultUnits = [{
+    id: 'ship1',
+    controllable: true,
+    startX: 20,
+    startY: 250,
+    height: 33, //px
+    width: 43, //px
+    rate: 200, //px/s
+    health: 100,
+    weapon: {
+      damage: 15,
+      range: 120, //px
+      cooldown: 2000, //ms
+      target: 'npc'
+    },
+    selectKey: '1',
+    attackKey: 'a',
+    type: 'pc',
+    img: 'images/green_ship.png'
+},{
+    id: 'healer',
+    controllable: true,
+    startX: 50,
+    startY: 300,
+    height: 36, //px
+    width: 43, //px
+    rate: 200, //px/s
+    health: 100,
+    weapon: {
+      damage: -5,
+      range: 120, //px
+      cooldown: 2500, //ms
+      target: 'pc'
+    },
+    selectKey: '2',
+    attackKey: 'a',
+    type: 'pc',
+    img: 'images/green_healer.png'
+}];
